7d51a0ee91144ded14626724673b2a3f8a0a4c40
neme-sis <ghoshsayan894@gmail.com>
2023-04-11 16:35:19 +0530

    folder structuring

--- worldchat/src/App.js
+++ worldchat/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./styles/App.css";
 
-import SignIn from './SignIn';
-import Chat from './Chat';
+import SignIn from "./components/SignIn";
+import Chat from "./components/Chat";
 
-import firebase from 'firebase/compat/app';  //sdk import
-import 'firebase/compat/firestore'   //for the database
-import 'firebase/compat/auth'    //for user authentication
-import {useAuthState} from 'react-firebase-hooks/auth';   //auth hook 
-import SignOut from './SignOut'
+import firebase from "firebase/compat/app"; //sdk import
+import "firebase/compat/firestore"; //for the database
+import "firebase/compat/auth"; //for user authentication
+import { useAuthState } from "react-firebase-hooks/auth"; //auth hook
+import SignOut from "./components/SignOut";
 
 firebase.initializeApp({
   apiKey: "AIzaSyDDp_Kf6fcm5uPYWtET9C98ZRuGpcLnXfo",
@@ -17,27 +17,29 @@ firebase.initializeApp({
   storageBucket: "worldchat-demo.appspot.com",
   messagingSenderId: "671810641127",
   appId: "1:671810641127:web:64f8665408fa7085b15642",
-  measurementId: "G-956ZM5MKF3"
-})
+  measurementId: "G-956ZM5MKF3",
+});
 
 const auth = firebase.auth();
 
-
 function App() {
   const [user] = useAuthState(auth);
-  
+
   return (
     <div className="App">
-      {user &&
-      <header className="App-header">
-      <h2>We Chat 
-      <img src={firebase.auth().currentUser.photoURL} alt="" />
-        <span><SignOut/></span>
-      </h2>
-    </header>}
+      {user && (
+        <header className="App-header">
+          <h2>
+            We Chat
+            <img src={firebase.auth().currentUser.photoURL} alt="" />
+            <span>
+              <SignOut />
+            </span>
+          </h2>
+        </header>
+      )}
 
-      {user?<Chat/>:<SignIn/>}
-
+      {user ? <Chat /> : <SignIn />}
     </div>
   );
 }
