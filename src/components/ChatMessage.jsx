51adf7ff29cfb4f908a2d0c350900b33c66e7b8d
neme-sis <ghoshsayan894@gmail.com>
2023-08-09 01:42:49 +0530

    chat message options and copy button

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,7 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, memo } from "react";
 import { auth } from "../config/firebaseConfig";
 import "../styles/ChatMessage.scss";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { AiOutlineCopy as CopyIcon } from "react-icons/ai";
+import { RiDeleteBin6Fill as DeleteIcon } from "react-icons/ri";
+import { MdOutlineAddReaction as ReactIcon } from "react-icons/md";
+import useToggle from "../hooks/useToggle";
+import { GlobalContext } from "../App";
+import { SUCCESS } from "../Types/AlertTypes";
+
+function copyToClipboard(text, setAlertData) {
+  navigator.clipboard.writeText(text);
+  // setAlertData({
+  //   type: SUCCESS,
+  //   title: "Successfully copied to clipboard",
+  //   isShowing: true,
+  // });
+}
 
 const ChatMessage = ({
   message,
@@ -21,6 +37,12 @@ const ChatMessage = ({
   if (hour < 10) hour = "0" + hour;
   if (minute < 10) minute = "0" + minute;
   const textClass = auth.currentUser.uid === uid ? "sent" : "recieved";
+
+  const messageOptionsMenuToggler = React.useRef(null);
+  const [messageOptions, toggleMessageOptions] = useToggle(
+    messageOptionsMenuToggler
+  );
+  const { setAlertData } = React.useContext(GlobalContext);
 
   function createObserver() {
     let options = {
@@ -47,6 +69,23 @@ const ChatMessage = ({
   }, [lastMessageRef.current]);
 
   const isShowUserDetail = !isSameDay || showDetail;
+  const optionsForMessage = [
+    {
+      name: <p>Copy</p>,
+      icon: <CopyIcon size={20} />,
+      onClick: () => copyToClipboard(text, setAlertData),
+    },
+    {
+      name: <p style={{ color: "#d00" }}>Delete</p>,
+      icon: <DeleteIcon size={20} color="#d00" />,
+      onClick: () => {},
+    },
+    {
+      name: <p>React</p>,
+      icon: <ReactIcon size={20} />,
+      onClick: () => {},
+    },
+  ];
 
   return (
     <Fragment>
@@ -61,7 +100,7 @@ const ChatMessage = ({
         className={`chat chat-${textClass} ${
           isLastMessage ? "last-message" : ""
         }`}
-        style={{ marginTop: isShowUserDetail ? "1.5rem" : "0.5rem" }}
+        style={{ marginTop: isShowUserDetail ? "1.25rem" : "0.25rem" }}
       >
         {isShowUserDetail && (
           <div className={`user-details user-details-${textClass}`}>
@@ -72,20 +111,45 @@ const ChatMessage = ({
           </div>
         )}
         <div className={`user-message user-message-${textClass}`}>
-          {text.length < textLengthLimit
-            ? text
-            : text.slice(0, textLengthLimit)}
-          {text.length > textLengthLimit && (
-            <span
-              className="read-more"
-              onClick={() => setTextLengthLimit((prev) => prev + textLimit)}
-            >
-              ...Read More
-            </span>
-          )}
+          <div className="message-content">
+            {text.length < textLengthLimit
+              ? text
+              : text.slice(0, textLengthLimit)}
+            {text.length > textLengthLimit && (
+              <span
+                className="read-more"
+                onClick={() => setTextLengthLimit((prev) => prev + textLimit)}
+              >
+                ...Read More
+              </span>
+            )}
+          </div>
           <div className="timestamp">
             {hour}:{minute}
           </div>
+          <div
+            className="three-dot"
+            onClick={toggleMessageOptions}
+            ref={messageOptionsMenuToggler}
+          >
+            <BsThreeDotsVertical />
+          </div>
+          {messageOptions && (
+            <div className="message-handling-options">
+              {optionsForMessage.map((opt, _) => (
+                <div
+                  className="message-handling-option"
+                  key={_}
+                  onClick={opt.onClick}
+                >
+                  {opt.icon && (
+                    <div className="message-option-icon">{opt.icon}</div>
+                  )}
+                  <div className="message-option-name">{opt.name}</div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </Fragment>
